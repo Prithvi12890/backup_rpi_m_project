f1a9316351f890acdff9c0a1b42e834c9fcb7ed4
Prithvi <prithvisandeep.saladi@gmail.com>
2023-01-21 19:49:42 +0000

    Soil moisture sensor added

--- major_project/major_project/app.py
+++ major_project/major_project/app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 app.debug = True # Make this False if you are no longer debugging
 
+moisture_level = ""
+water_pump = ""
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -34,7 +37,7 @@ def temp_hum_ui_home():
     import Adafruit_DHT
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 17)
     if humidity is not None and temperature is not None:
-        return render_template("dashboard.html",temp=temperature,hum=humidity)
+        return render_template("dashboard.html",temp=temperature,hum=humidity,moisture=moisture_level, change=change_of_var)
     else:
         return render_template("no_sensor.html")
 
@@ -44,7 +47,7 @@ def temp_hum_ui_home_dashboard():
     import Adafruit_DHT
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 17)
     if humidity is not None and temperature is not None:
-        return render_template("dashboard.html",temp=temperature,hum=humidity)
+        return render_template("dashboard.html",temp=temperature,hum=humidity,moisture=moisture_level,pump=water_pump)
     else:
         return render_template("no_sensor.html")
 
@@ -63,6 +66,30 @@ def tables():
     humidities = curs.fetchall()
     conn.close()
     return render_template("tables.html",temp=temperatures,hum=humidities)
+
+import RPi.GPIO as GPIO
+
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+GPIO.setup(26, GPIO.OUT)
+
+def callback(channel):
+        global moisture_level, water_pump
+        if GPIO.input(channel):
+                # print("no Water Detected!")
+                moisture_level = "Not Adequate"
+                GPIO.output(26, GPIO.HIGH)
+                water_pump = "ON"
+        else:
+                # print("Water Detected!")
+                moisture_level = "Adequate"
+                GPIO.output(26, GPIO.LOW)
+                water_pump = "OFF"
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
